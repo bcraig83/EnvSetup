3540391278c6bb621b3921d5166b281f46ad3173
Ben Craig <ben.craig@sita.aero>
2015-04-17 16:42:19 +0100

    Set up the script to use IE as the default browser. This is required if we're going to be running a boxstarter script from the web (which seems like the easiest approach for now). I'm assuming that people will be able to switch back to their own favourite browser afterwards; I don't want to spend ages writing a script that figures out what their current browser is and resets it afterwards.

--- BasicInstall.ps1
+++ BasicInstall.ps1
@@ -1,8 +1,17 @@
+function Set-DefaultBrowserToIE
+{
+    Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice' -name ProgId IE.HTTP
+    # Rather than waste time figuring out what browser was being used, I'm assuming that people will just switch back themselves the next time they use their browser...
+}
+
 # Install chocolatey
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install BoxStarter
 cinst boxstarter -y
 
-# Open BoxStarter Shell
-BoxstarterShell
+# Set Default Browser to IE. This is required for running boxstarter packages.
+Set-DefaultBrowserToIE
+
+START http://boxstarter.org/package/nr/url?
+
