780e4b16a8aa830db7d06f7de860285448018c26
Ben Craig <ben.craig@sita.aero>
2015-04-17 14:26:56 +0100

    Move the main commands out into a powershell script and wrap it inside a single bat file.

--- BasicInstall.bat
+++ BasicInstall.bat
@@ -1,1 +1,32 @@
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+@echo off
+
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+echo %CD%
+
+echo -------------------
+echo    Basic Install
+echo -------------------
+
+powershell Set-ExecutionPolicy RemoteSigned >nul
+if not errorlevel 1 goto psok
+powershell -h >nul 2>&1
+if errorlevel 1 goto nops
+echo.
+echo Not running as Administrator...
+echo.
+pause
+EXIT
+
+:psok
+powershell -File BasicInstall.ps1
+pause
+EXIT
+
+:nops
+echo.
+echo ERROR: Can't find Powershell!
+echo.
+pause
+EXIT
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EnvSetup
 A collection of scripts that allow the automated generation of various environments (developer environments, test environments, deployment environments, etc)
+
+# Notes
+BasicInstall.bat needs to be run as an Administrator.
