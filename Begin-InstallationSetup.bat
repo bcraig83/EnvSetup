a10afe71d1dedb7a3205849ad81cbd5e34ed4f6b
sod80 <sodonnell80@gmail.com>
2015-04-24 14:15:59 +0100

    This will automatically startup an Admin cmd window and disable UAC for the session

--- Begin-InstallationSetup.bat
+++ Begin-InstallationSetup.bat
@@ -3,9 +3,45 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
 
-echo -------------------
-echo    Basic Install
-echo -------------------
+:::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights
+:::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS 
+ECHO.
+ECHO =============================
+ECHO Starting up Admin shell
+ECHO Disable UAC
+ECHO Set-ExecutionPolicy Unrestricted
+ECHO =============================
+ 
+:checkPrivileges 
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges ) 
+ 
+:getPrivileges 
+if '%1'=='ELEV' (shift & goto gotPrivileges)  
+ECHO. 
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation 
+ECHO **************************************
+ 
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs" 
+ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs" 
+"%temp%\OEgetPrivileges.vbs" 
+exit /B 
+ 
+:gotPrivileges 
+::::::::::::::::::::::::::::
+:START
+::::::::::::::::::::::::::::
+setlocal & pushd .
+ 
+REM Run shell as admin (example)
+::::::::::::::::::::::::::::
 
 powershell Set-ExecutionPolicy Unrestricted >nul
 if not errorlevel 1 goto psok
@@ -21,8 +57,10 @@ EXIT
 powershell -File Install-EnvSetup.ps1
 pause
 
-:psok
 powershell -File Install-Software.ps1
+ECHO =============================
+echo    Exiting Basic Install
+ECHO =============================
 pause
 
 EXIT
